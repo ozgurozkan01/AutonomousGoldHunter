540dd4fe2b08b6f94d32687c5a3e174db036d543
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-03-16 21:37:16 +0300

    update code by replace enum

--- src/main/java/com/example/prolab2_1/ObstacleGenerator.java
+++ src/main/java/com/example/prolab2_1/ObstacleGenerator.java
@@ -10,7 +10,7 @@ public class ObstacleGenerator {
     TreeObstacles[] treeObstacles = TreeObstacles.values();
     RockObstacles[] rockObstacles = RockObstacles.values();
 
-    // Create Two Times From Whole Obstacles
+    // Creates Two Times From Whole Obstacles
     public void createDefaultObstacles(ArrayList<StaticObstacle> staticObstacles) throws FileNotFoundException {
         for (int i = 0; i < 2; i++) {
             for (TypeObstacles typeObstacles : TypeObstacles.values()) {
@@ -23,28 +23,28 @@ public class ObstacleGenerator {
     public StaticObstacle generateSummerObstacle(TypeObstacles typeObstacles) throws FileNotFoundException {
         switch (typeObstacles) {
             case WALL:
-                return new StaticObstacle("pictures/wall_summer.jpg", 10, 1, "summer", TypeObstacles.WALL);
+                return new StaticObstacle("pictures/wall_summer.jpg", 10, 1, Season.SUMMER, TypeObstacles.WALL);
             case MOUNTAIN:
-                return new StaticObstacle("pictures/mountain_summer.png", 15, 15, "summer", TypeObstacles.MOUNTAIN);
+                return new StaticObstacle("pictures/mountain_summer.png", 15, 15, Season.SUMMER, TypeObstacles.MOUNTAIN);
             case TREE:
                 randomIndex = random.nextInt(TreeObstacles.values().length);
                 switch (treeObstacles[randomIndex]) {
                     case TREE2X2:
-                        return new StaticObstacle("pictures/tree_summer_2x2.png", 2, 2, "summer", TypeObstacles.TREE);
+                        return new StaticObstacle("pictures/tree_summer_2x2.png", 2, 2, Season.SUMMER, TypeObstacles.TREE);
                     case TREE3X3:
-                        return new StaticObstacle("pictures/tree_summer_3x3.png", 3, 3, "summer", TypeObstacles.TREE);
+                        return new StaticObstacle("pictures/tree_summer_3x3.png", 3, 3, Season.SUMMER, TypeObstacles.TREE);
                     case TREE4X4:
-                        return new StaticObstacle("pictures/tree_summer_4x4.png", 4, 4, "summer", TypeObstacles.TREE);
+                        return new StaticObstacle("pictures/tree_summer_4x4.png", 4, 4, Season.SUMMER, TypeObstacles.TREE);
                     case TREE5X5:
-                        return new StaticObstacle("pictures/tree_summer_5x5.png", 5, 5, "summer", TypeObstacles.TREE);
+                        return new StaticObstacle("pictures/tree_summer_5x5.png", 5, 5, Season.SUMMER, TypeObstacles.TREE);
                 }
             case ROCK:
                 randomIndex = random.nextInt(RockObstacles.values().length);
                 switch (rockObstacles[randomIndex]) {
                     case ROCK2X2:
-                        return new StaticObstacle("pictures/rock_summer_2x2.png", 2, 2, "summer", TypeObstacles.ROCK);
+                        return new StaticObstacle("pictures/rock_summer_2x2.png", 2, 2, Season.SUMMER, TypeObstacles.ROCK);
                     case ROCK3X3:
-                        return new StaticObstacle("pictures/rock_summer_3x3.png", 3, 3, "summer", TypeObstacles.ROCK);
+                        return new StaticObstacle("pictures/rock_summer_3x3.png", 3, 3, Season.SUMMER, TypeObstacles.ROCK);
                 }
         }
         return null;
@@ -53,28 +53,28 @@ public class ObstacleGenerator {
     public StaticObstacle generateWinterObstacle(TypeObstacles typeObstacles) throws FileNotFoundException {
         switch (typeObstacles) {
             case WALL:
-                return new StaticObstacle("pictures/wall_winter.png", 10, 1, "winter", TypeObstacles.WALL);
+                return new StaticObstacle("pictures/wall_winter.png", 10, 1, Season.WINTER, TypeObstacles.WALL);
             case MOUNTAIN:
-                return new StaticObstacle("pictures/mountain_winter.png", 15, 15, "winter", TypeObstacles.MOUNTAIN);
+                return new StaticObstacle("pictures/mountain_winter.png", 15, 15, Season.WINTER, TypeObstacles.MOUNTAIN);
             case TREE:
                 randomIndex = random.nextInt(TreeObstacles.values().length);
                 switch (treeObstacles[randomIndex]) {
                     case TREE2X2:
-                        return new StaticObstacle("pictures/tree_winter_2x2.png", 2, 2, "winter", TypeObstacles.TREE);
+                        return new StaticObstacle("pictures/tree_winter_2x2.png", 2, 2, Season.WINTER, TypeObstacles.TREE);
                     case TREE3X3:
-                        return new StaticObstacle("pictures/tree_winter_3x3.png", 3, 3, "winter", TypeObstacles.TREE);
+                        return new StaticObstacle("pictures/tree_winter_3x3.png", 3, 3, Season.WINTER, TypeObstacles.TREE);
                     case TREE4X4:
-                        return new StaticObstacle("pictures/tree_winter_4x4.png", 4, 4, "winter", TypeObstacles.TREE);
+                        return new StaticObstacle("pictures/tree_winter_4x4.png", 4, 4, Season.WINTER, TypeObstacles.TREE);
                     case TREE5X5:
-                        return new StaticObstacle("pictures/tree_winter_5x5.png", 5, 5, "winter", TypeObstacles.TREE);
+                        return new StaticObstacle("pictures/tree_winter_5x5.png", 5, 5, Season.WINTER, TypeObstacles.TREE);
                 }
             case ROCK:
                 randomIndex = random.nextInt(RockObstacles.values().length);
                 switch (rockObstacles[randomIndex]) {
                     case ROCK2X2:
-                        return new StaticObstacle("pictures/rock_winter_2x2.png", 2, 2, "winter", TypeObstacles.ROCK);
+                        return new StaticObstacle("pictures/rock_winter_2x2.png", 2, 2, Season.WINTER, TypeObstacles.ROCK);
                     case ROCK3X3:
-                        return new StaticObstacle("pictures/rock_winter_3x3.png", 3, 3, "winter", TypeObstacles.ROCK);
+                        return new StaticObstacle("pictures/rock_winter_3x3.png", 3, 3, Season.WINTER, TypeObstacles.ROCK);
                 }
         }
         return null;
@@ -83,9 +83,9 @@ public class ObstacleGenerator {
     public DynamicObstacle generateDynamicObstacle (DynamicObstacles dynamicObstacle) throws FileNotFoundException {
         switch (dynamicObstacle){
             case BEE:
-                return new DynamicObstacle("pictures/bee.png", 3, 2, DynamicObstacles.BEE);
+                return new DynamicObstacle("pictures/bee.png", 5, 2,2, 2, DynamicObstacles.BEE);
             case BIRD:
-                return new DynamicObstacle("pictures/bird.png", 2, 5, DynamicObstacles.BIRD);
+                return new DynamicObstacle("pictures/bird.png", 2, 9, 2, 2, DynamicObstacles.BIRD);
         }
         return null;
     }
