e76ab0a34e9f2b6f9fb6c1aabfc7ecec2fd923be
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-03-16 21:34:41 +0300

    refactor code to make more readable

--- src/main/java/com/example/prolab2_1/DynamicObstacle.java
+++ src/main/java/com/example/prolab2_1/DynamicObstacle.java
@@ -14,27 +14,31 @@ enum DynamicObstacles {
 }
 
 public class DynamicObstacle extends ObstacleBase{
-    Enum species;
     private TranslateTransition translateObstacle = new TranslateTransition();
+    Enum species;
+    int visitFieldX;
+    int visitFieldY;
 
-    DynamicObstacle (String imagePath, int sizeX, int sizeY, Enum species) throws FileNotFoundException {
+    DynamicObstacle (String imagePath, int visitFieldX, int visitFieldY,int sizeX, int sizeY, Enum species) throws FileNotFoundException {
         this.sizeX = sizeX;
         this.sizeY = sizeY;
+        this.visitFieldX = visitFieldX;
+        this.visitFieldY = visitFieldY;
         this.species = species;
         this.imagePath = new FileInputStream(imagePath);
         image = new Image(this.imagePath);
         imageView = new ImageView(image);
-        imageView.setFitHeight(2 * 10);
-        imageView.setFitWidth(2 * 10);
+        imageView.setFitHeight(this.sizeY * HelloApplication.rectangleAndGapSize);
+        imageView.setFitWidth(this.sizeX * HelloApplication.rectangleAndGapSize);
         translateObstacle.setNode(imageView);
-        translateObstacle.setDuration(Duration.millis(1000));
+        translateObstacle.setDuration(Duration.millis(1500));
         translateObstacle.setCycleCount(TranslateTransition.INDEFINITE);
         translateObstacle.setAutoReverse(true);
 
         if (species == DynamicObstacles.BEE)
-            translateObstacle.setByX(10);
+            translateObstacle.setByX((visitFieldX - sizeX) * HelloApplication.rectangleAndGapSize);
         else
-            translateObstacle.setByY(30);
+            translateObstacle.setByY((visitFieldY - sizeY) * HelloApplication.rectangleAndGapSize);
 
         translateObstacle.play();
     }
